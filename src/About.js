4916df5f62d136287d3e9bb208491be680eca564
aishabraud92 <aishabraud@gmail.com>
2018-01-18 21:31:55 -0800

    Add files

---
+++ src/About.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+class About extends Component {
+  render() {
+    return (
+      <div className="About">
+        <h1>About Me</h1>
+          <p>My Life In Words...</p>
+      </div>
+    );
+  }
+}
+export default About;
---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <h1>Welcome!</h1>
+          <p>Explore My Blog</p>
+      </div>
+    );
+  }
+}
+export default Home;
